74061fa61830918f72b4d08259f156a740f2bb8a
b1q34war <b1q34war@gmail.com>
2021-03-21 17:55:30 +0300

    Выполнил исправления

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,10 @@ let closePopupBtn = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__discript');
 
-// Выберем DOM .popup__form-name и .popup__form-about
+// Выберем DOM popup__input_value_name и .popup__input_value_job
 // для  дальнейшего изменения значений формы и их передачу
-let formName = document.querySelector('.popup__form-name');
-let formJob = document.querySelector('.popup__form-about');
+let formName = document.querySelector('.popup__input_value_name');
+let formJob = document.querySelector('.popup__input_value_job');
 
 // Выберем кнопку сохрарнения
 let saveButton = document.querySelector('.popup__form-button');
@@ -21,17 +21,11 @@ let formElement = document.querySelector('.popup__form');
 
 //Создаем функцию  по открытию popup через 
 //(добавление класса) и при открытии  передаем в форму значения аргумента. 
-function openPopup(popupName, popupjob) {
+function openPopup() {
   popup.classList.add('popup_opened');
-  document.querySelector('.popup__form-name').value = popupName;
-  document.querySelector('.popup__form-about').value = popupjob;
+  formName.value = profileName.textContent;
+  formJob.value = profileJob.textContent;
 }
-
-//Вешаем слушатель на конпку 
-//выполняем функцию в которую передаем аргументы текста из профиля. 
-openPopupBtn.addEventListener('click', function() {
-  openPopup(profileName.textContent, profileJob.textContent);
-});
 
 //Создаем функцию  по закрытию popup через 
 //(удаление класса), так как при нажатии крестика, сихранение и отправка формы 
@@ -39,11 +33,6 @@ openPopupBtn.addEventListener('click', function() {
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
-
-//Вешаем слушатель на кнопку креста и выполняем функцию по закрытию.
-closePopupBtn.addEventListener('click', function() {
-  closePopup();
-});
 
 //Создаем функцию по получению данных из значений форм и их отправке
 // + обнуляем стандартную форму отправки
@@ -53,6 +42,13 @@ function formSubmitHandler (evt) {
   profileJob.textContent = formJob.value;
   closePopup();
 }
+
+//Вешаем слушатель на конпку 
+//выполняем функцию в которую передаем аргументы текста из профиля. 
+openPopupBtn.addEventListener('click', openPopup);
+
+//Вешаем слушатель на кнопку креста и выполняем функцию по закрытию.
+closePopupBtn.addEventListener('click', closePopup);
 
 //Вешаем слушатель на форму по действию submit (нажатие enter и\или click по конпке формы)
 formElement.addEventListener('submit', formSubmitHandler);
